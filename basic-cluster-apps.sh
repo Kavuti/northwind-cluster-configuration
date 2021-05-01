a4c639499fde5c32a9c84508b1e1d40dbdaa5149
Kavuti <christian.cavuti@gmail.com>
2021-05-02 00:02:06 +0200

    Adding base cluster configuration

--- basic-cluster-apps.sh
+++ basic-cluster-apps.sh
@@ -1,3 +1,3 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
-
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
 kubectl create ns keycloak
